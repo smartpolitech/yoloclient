4ff93a9d06547c312eefa54f75e85a32e17f38e1
Pablo Bustos <pbustos@unex.es>
2017-05-16 16:39:31 +0200

    script de prueba pra captura de video

---
+++ src/prueba.py
@@ -0,0 +1,43 @@
+#
+# Copyright (C) 2017 by YOUR NAME HERE
+#
+#    This file is part of RoboComp
+#
+#    RoboComp is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    RoboComp is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with RoboComp.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+import sys, os, traceback, time
+import numpy as np
+import cv2
+
+cv2.namedWindow("YOLO IA", cv2.WINDOW_AUTOSIZE)
+
+try:
+	# self.vidFile = cv2.VideoCapture("http://158.49.247.240:88/cgi-bin/CGIStream.cgi?cmd=GetMJStream&usr=admin&pwd=opticalflow")
+	self.vidFile = cv2.VideoCapture("/dev/video0")
+	# self.vidFile = cv2.VideoCapture("http://158.49.247.184:88/cgi-bin/CGIStream.cgi?cmd=GetMJStream&usr=admin&pwd=opticalflow")
+except:
+	print "problem opening input stream"
+	sys.exit(1)
+
+if not self.vidFile.isOpened():
+	print "capture stream not open"
+	sys.exit(1)
+
+while True:
+	ret, frame = self.vidFile.read()  # read first frame, and the return code of the function.
+	if ret:
+		self.processFrame(frame)
+		cv2.imshow('Image', frame);
+		cv2.waitKey(20);
